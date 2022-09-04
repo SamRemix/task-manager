39b7423f093af922206dc1a49fcd29ea3977c16a
SamRemix <s_capron@hotmail.fr>
2022-09-04 20:16:35 +0200

    home

--- front_/src/pages/Home.js
+++ front_/src/pages/Home.js
@@ -5,45 +5,46 @@ const Home = () => {
     <section className="container home-page">
       <motion.h1
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: .6, ease: 'easeOut' } }}
-        exit={{ opacity: 0, transition: { duration: .4, ease: 'easeOut' } }}>
+        animate={{ opacity: 1, transition: { duration: .6 } }}
+        exit={{ opacity: 0, transition: { duration: .4 } }}>
         Home
       </motion.h1>
 
       <motion.div
         className="content"
         initial={{ y: 80, opacity: 0 }}
-        animate={{ y: 0, opacity: 1, transition: { duration: .6, ease: 'easeOut' } }}
-        exit={{ y: 80, opacity: 0, transition: { duration: .4, ease: 'easeOut' } }}>
+        animate={{ y: 0, opacity: 1, transition: { duration: .6 } }}
+        exit={{ y: 80, opacity: 0, transition: { duration: .4 } }}>
         <div className="git">
-          <div className="commands">
-            <h2>git init</h2>
-            <p>Initialize an existing directory as a Git repository</p>
-          </div>
-          {/* <div className="commands">
-            <h2>git remote add <i>origin github-repo-link</i></h2>
-            <p><span>alias: origin, URL: github-repo-link</span></p>
-            <p>Add a git URL as an alias</p>
-          </div> */}
-          <div className="commands">
-            <h2>git add .</h2>
-            <p><span>git add folder/file: single file</span></p>
-            <p>Add a file as it looks now to your next commit (stage)</p>
-          </div>
-          <div className="commands">
-            <h2>git status</h2>
-            <p>Show modified files in working directory, staged for your next commit</p>
-          </div>
-          <div className="commands">
-            <h2>git commit <i>-m "comment"</i></h2>
-            <p><span>add comment: -m "comment"</span></p>
-            <p>commit your staged content as a new commit snapshot</p>
-          </div>
-          <div className="commands">
-            <h2>git push</h2>
-            {/* <h2>git push -u <i>origin branch</i></h2> */}
-            <p><span>origin: alias (git remote add), branch: master</span></p>
-            <p>Transmit local branch commits to the remote repository branch</p>
+          <div className="commands-container">
+            <div className="commands">
+              <p className="command-name">git init</p>
+              <p>Initialize an existing directory as a Git repository</p>
+            </div>
+            <div className="commands">
+              <p className="command-name">git remote add origin https://github.com/SamRemix/task-manager-MERN.git</p>
+              <p>connect the project to a repository with an alias</p>
+            </div>
+            <div className="commands">
+              <p className="command-name">git add .</p>
+              <p>Add all modified files to the next commit</p>
+              <p>if error "(fetch first)" =&gt; git fetch</p>
+              <p>if error "(non-fast-forward)" =&gt; git pull</p>
+            </div>
+            <div className="commands">
+              <p className="command-name">git status</p>
+              <p>Show current branch & modified files</p>
+            </div>
+            <div className="commands">
+              <p className="command-name">git commit -m "comment"</p>
+              <p>after git add</p>
+              <p>commit files and add a comment about it</p>
+            </div>
+            <div className="commands">
+              <p className="command-name">git push</p>
+              {/* <h2>git push origin main</h2> */}
+              <p>Transmit local branch commits to the remote repository branch</p>
+            </div>
           </div>
         </div>
       </motion.div>
