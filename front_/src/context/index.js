4a84c7576cadc4ad41d738752e3dd5ca61099b44
SamRemix <s_capron@hotmail.fr>
2022-12-20 00:14:48 +0100

    review

--- front_/src/context/index.js
+++ front_/src/context/index.js
@@ -1,20 +1,16 @@
 import { AuthContextProvider } from './AuthContext'
 // import { TasksContextProvider } from './TasksContext'
 // import { BoardsContextProvider } from './BoardsContext'
-import { BoardsContext, useBoardsReducer } from './BoardsContext'
 import { TasksContext, useTasksReducer } from './TasksContext'
 
 const Context = ({ children }) => {
-  const [boards, dispatchBoards] = useBoardsReducer()
   const [tasks, dispatchTasks] = useTasksReducer()
 
   return (
     <AuthContextProvider>
-      <BoardsContext.Provider value={{ ...boards, dispatchBoards }}>
-        <TasksContext.Provider value={{ ...tasks, dispatchTasks }}>
-          {children}
-        </TasksContext.Provider>
-      </BoardsContext.Provider>
+      <TasksContext.Provider value={{ ...tasks, dispatchTasks }}>
+        {children}
+      </TasksContext.Provider>
     </AuthContextProvider>
   );
 }
