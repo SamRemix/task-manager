63ca665854c835eac837e44f0783fb522f6e0450
SamRemix <s_capron@hotmail.fr>
2023-03-01 13:39:18 +0100

    ...

--- front_/src/hooks/useAxios.js
+++ front_/src/hooks/useAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const useAxios = token => {
   const instance = axios.create({
-    baseURL: 'https://task-manager-mern-back.vercel.app',
+    // baseURL: 'https://task-manager-mern-back.vercel.app',
     headers: {
       Authorization: `Bearer ${token}`
     }
